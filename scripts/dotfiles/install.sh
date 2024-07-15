fa0aae48553042704911c65b887cc47741ff6121
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-07-15 16:33:55 -0700

    install.sh: added fortune, considering pre-commit

--- scripts/dotfiles/install.sh
+++ scripts/dotfiles/install.sh
@@ -47,10 +47,11 @@ declare -A packages
 packages[dev_tools]="openjdk-21-jdk gcc g++ clang ninja-build cmake shellcheck build-essential gdb iwyu cpplint hyperfine lldb python3 python3-venv"
 packages[utilities]="tealdeer unzip eza bat jq ripgrep fzf x11-apps mesa-utils htop btop screen dos2unix fd-find dasel bind9-dnsutils"
 packages[theming]="gnome-themes-extra lxappearance" # TODO: make intall & update script for Catppuiccin
-packages[fun]="sl neofetch hollywood"
+packages[fun]="sl neofetch hollywood fortune-mod"
 packages[wsl]="wslu" # TODO: add a check to only install on WSL
 packages[tor]="tor tor-geoipdb torsocks"
 packages[doc]="bash-doc"
+# maybe: pre-commit
 
 # shellcheck disable=SC2086
 sudo apt install ${packages[dev_tools]} \
