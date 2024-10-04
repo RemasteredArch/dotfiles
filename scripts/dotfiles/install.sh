263459371eef5e4c7905b8dc49979dcfb80dbe02
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2024-10-04 07:29:36 -0700

    `install.sh`: apt packages: removed fd-find (already through cargo), added mingw-w64

--- scripts/dotfiles/install.sh
+++ scripts/dotfiles/install.sh
@@ -44,8 +44,8 @@ sudo snap refresh
 
 announce "Installing various packages"
 declare -A packages
-packages[dev_tools]="openjdk-21-jdk gcc g++ clang ninja-build cmake shellcheck build-essential gdb iwyu cpplint hyperfine lldb python3 python3-venv"
-packages[utilities]="tealdeer unzip eza bat jq ripgrep fzf x11-apps mesa-utils htop btop screen dos2unix fd-find dasel bind9-dnsutils pandoc"
+packages[dev_tools]="openjdk-21-jdk gcc g++ clang ninja-build cmake shellcheck build-essential gdb iwyu cpplint hyperfine lldb python3 python3-venv mingw-w64"
+packages[utilities]="tealdeer unzip eza bat jq ripgrep fzf x11-apps mesa-utils htop btop screen dos2unix dasel bind9-dnsutils pandoc"
 packages[theming]="gnome-themes-extra lxappearance" # TODO: make intall & update script for Catppuiccin
 packages[fun]="sl neofetch hollywood fortune-mod cowsay"
 packages[wsl]="wslu" # TODO: add a check to only install on WSL
